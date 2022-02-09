8ce77c6024299650e983249cc3f2e97c8e9adbf0
Sean Aguinaga <aguinaga.sean@gmail.com>
2022-02-09 12:41:58 -0800

    hmm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,39 +17,13 @@ let preloadedQuery = loadQuery<indexPage_indexQuery>(
 );
 export default function Home() {
   return (
-    <ErrorBoundary>
+    <>
       <Link href="/rsc" passHref>
         <ion-item>RSC</ion-item>
       </Link>
       <Suspense fallback="loading blocks">
         <Blocks queryReference={preloadedQuery} />
       </Suspense>
-    </ErrorBoundary>
+    </>
   );
-}
-
-class ErrorBoundary extends React.Component<any, { hasError: boolean }> {
-  constructor(props: any) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError(error: Error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: any) {
-    // You can also log the error to an error reporting service
-    console.log(error, errorInfo);
-  }
-
-  render() {
-    if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
-    }
-
-    return this.props.children;
-  }
 }
